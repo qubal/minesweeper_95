403d23b0d83ec5c843490c5207075449a6faf000
VALENTIN KAZANCEV <k.val.psn@gmail.com>
2020-04-14 12:53:23 +0700

    Updates

---
+++ precache-manifest.42d655bb1fb9abfcfcb1e58d496741bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e04b3cde96343b7ec9ea7a504d8951",
+    "url": "/minesweeper_95/index.html"
+  },
+  {
+    "revision": "3dcfdfef4c9c3b15dea4",
+    "url": "/minesweeper_95/static/css/main.4d8649b4.chunk.css"
+  },
+  {
+    "revision": "f62dfb7facda768cdf1b",
+    "url": "/minesweeper_95/static/js/2.72dc1ed7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper_95/static/js/2.72dc1ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dcfdfef4c9c3b15dea4",
+    "url": "/minesweeper_95/static/js/main.4055ee1a.chunk.js"
+  },
+  {
+    "revision": "b28d6f6e28455c683a0b",
+    "url": "/minesweeper_95/static/js/runtime-main.98774c49.js"
+  }
+]);
